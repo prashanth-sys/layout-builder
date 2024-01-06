b6e20b6fd5d1fd3ed8836ebf1992d6ad4aa6a558
Your Name <reddyprashanth512@gmail.com>
2024-01-06 12:14:47 +0000

    solution

--- src/components/ConfigurationController/index.js
+++ src/components/ConfigurationController/index.js
@@ -1,78 +1,75 @@
-// Write your code here
-import {Component} from 'react'
-
+import ConfigurationContext from '../../context/ConfigurationContext'
 import './index.css'
 
-class ConfigurationController extends Component {
-  state = {
-    showContent: true,
-    showLeftNavbar: true,
-    showRightNavbar: true,
-  }
-
-  onChangeContent = () => {
-    const {showContent} = this.state
-    this.setState(prevState => ({
-      showContent: !prevState.showContent,
-    }))
-    console.log(`${showContent} triggered`)
-  }
-
-  onChangeLeftNav = () => {
-    const {showLeftNavbar} = this.state
-    this.setState(prevState => ({
-      showLeftNavbar: !prevState.showLeftNavbar,
-    }))
-    console.log(`${showLeftNavbar} triggered`)
-  }
-
-  onChangeRightNav = () => {
-    const {showRightNavbar} = this.state
-    this.setState(prevState => ({
-      showRightNavbar: !prevState.showRightNavbar,
-    }))
-    console.log(`${showRightNavbar} triggered`)
-  }
-
-  render() {
-    const {showContent, showLeftNavbar, showRightNavbar} = this.state
-    return (
-      <div className="bg-container">
-        <h1 className="main-heading">Layout</h1>
-        <div className="checkbox-container">
-          <label className="label" htmlFor="content">
-            <input
-              type="checkbox"
-              className="checkbox"
-              id="content"
-              onChange={this.onChangeContent}
-              checked={showContent}
-            />
-            Content
-          </label>
-          <label className="label" htmlFor="left">
-            <input
-              type="checkbox"
-              className="checkbox"
-              id="left"
-              onChange={this.onChangeLeftNav}
-              checked={showLeftNavbar}
-            />
-            Left Navbar
-          </label>
-          <label className="label" htmlFor="right">
-            <input
-              type="checkbox"
-              className="checkbox"
-              id="right"
-              onChange={this.onChangeRightNav}
-              checked={showRightNavbar}
-            />
-            Right Navbar
-          </label>
-        </div>
-      </div>
-    )
-  }
+const content = {
+  showContent: true,
+  showLeftNavbar: true,
+  showRightNavbar: true,
 }
+
+const ConfigurationController = () => (
+  <ConfigurationContext.Consumer>
+    {value => {
+      const {
+        showContent,
+        showLeftNavbar,
+        showRightNavbar,
+        onToggleShowContent,
+        onToggleShowRightNavbar,
+        onToggleShowLeftNavbar,
+      } = value
+      const onChangeContent = () => {
+        onToggleShowContent(content.showContent)
+        console.log('')
+      }
+      const onChangeLeftNav = () => {
+        onToggleShowRightNavbar(content.showLeftNavbar)
+        console.log('')
+      }
+      const onChangeRightNav = () => {
+        onToggleShowLeftNavbar(content.showRightNavbar)
+        console.log('')
+      }
+
+      return (
+        <div className="bg-container">
+          <h1 className="main-heading">Layout</h1>
+          <div className="checkbox-container">
+            <label className="label" htmlFor="content">
+              <input
+                type="checkbox"
+                className="checkbox"
+                id="content"
+                onChange={onChangeContent}
+                value={showContent}
+              />
+              Content
+            </label>
+            <label className="label" htmlFor="left">
+              <input
+                type="checkbox"
+                className="checkbox"
+                id="left"
+                onChange={onChangeLeftNav}
+                value={showLeftNavbar}
+              />
+              Left Navbar
+            </label>
+            <label className="label" htmlFor="right">
+              <input
+                type="checkbox"
+                className="checkbox"
+                id="right"
+                onChange={onChangeRightNav}
+                value={showRightNavbar}
+              />
+              Right Navbar
+            </label>
+          </div>
+        </div>
+      )
+    }}
+  </ConfigurationContext.Consumer>
+)
+
 export default ConfigurationController
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,37 +1,52 @@
-import {Component} from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
 import Body from '../Body'
+import ConfigurationContext from '../../context/ConfigurationContext'
+
 import './index.css'
 
-class Layout extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <div className="layout-container">
-          <div className="left-navbar">
-            <h1 className="left-heading">Left Navbar Menu</h1>
-            <p className="item">Item 1</p>
-            <p className="item">Item 2</p>
-            <p className="item">Item 3</p>
-            <p className="item">Item 4</p>
+const Layout = () => (
+  <ConfigurationContext.Consumer>
+    {value => {
+      const {showContent, showLeftNavbar, showRightNavbar} = value
+      return (
+        <>
+          <Header />
+          <div className="layout-container">
+            {showLeftNavbar ? (
+              <div className="left-navbar">
+                <h1 className="left-heading">Left Navbar Menu</h1>
+                <ul>
+                <li className="item">Item 1</li>
+                <li className="item">Item 2</li>
+                <li className="item">Item 3</li>
+                <li className="item">Item 4</li>
+                <li className="item">Item 5</li>
+              </ul>
+              </div>
+            
+            ) : null}
+
+            {showContent ? (
+              <div className="content-container">
+                <h1 className="content-heading">Content</h1>
+                <p className="content-description">
+                  Lorem ipsum dolor sit amet, shiva parvathi Hare Rama Rama Rama
+                  Hare Hare Hare Krishna Krishna Krishna Hare Hare
+                </p>
+              </div>
+            ) : null}
+            {showRightNavbar ? (
+              <div className="right-container">
+                <h1 className="right-heading">Right Navbar</h1>
+                <Body />
+              </div>
+            ) : null}
           </div>
-          <div className="content-container">
-            <h1 className="content-heading">Content</h1>
-            <p className="content-description">
-              Lorem ipsum dolor sit amet, shiva parvathi Hare Rama Rama Rama
-              Hare Hare Hare Krishna Krishna Krishna Hare Hare
-            </p>
-          </div>
-          <div className="right-container">
-            <h1 className="right-heading">Right Navbar</h1>
-            <Body />
-          </div>
-        </div>
-        <Footer />
-      </>
-    )
-  }
-}
+          <Footer />
+        </>
+      )
+    }}
+  </ConfigurationContext.Consumer>
+)
 export default Layout
